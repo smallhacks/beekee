f594bdcb6a242ff2ecf493c6d4dea6d37d80bc54
Vincent Widmer <eodecorp@hotmail.com>
2018-08-01 14:18:30 +0200

    Validate space user form

--- src/client/templates/space/space_users.js
+++ src/client/templates/space/space_users.js
@@ -1,9 +1,25 @@
+Template.spaceUsers.onRendered(function() {
+
+	$(".space-users--form").validate({
+        rules: {
+            userName: {
+                required: true,
+                minlength: 1,
+                maxlength: 15
+            }
+        }
+    }); 
+
+    Session.set('numChars', 0); // Count the number of characters
+});
+
+
 Template.spaceUsers.events({
 	
 	'submit form': function(event, template) {
 		event.preventDefault();
 
-		var authorName = $('#space-users--input').val().trim();
+		var authorName = $('#userName').val().trim();
 
 		if (authorName != "") {
 			if (Authors.findOne({name:authorName})) {
@@ -29,5 +45,16 @@ Template.spaceUsers.events({
 	'click .space-users--button-submit-author': function(event, template) {
 		event.preventDefault();
 		$('#space-users--form').submit();
-	}
+	},
+	'input #userName': function(){
+    	Session.set('numChars', $('#userName').val().length);
+  	}
+});
+
+
+Template.spaceUsers.helpers({
+
+	'numChars': function(menuItemId) {
+		return Session.get('numChars');
+	},
 });
