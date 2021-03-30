5a01bfed7cb0b264df8e3fd87693720876ae966c
Vincent Widmer <eodecorp@hotmail.com>
2021-03-30 14:25:39 +0200

    Add License in each files

--- src/client/templates/space/home/home_post.js
+++ src/client/templates/space/home/home_post.js
@@ -1,3 +1,23 @@
+// Copyright 2016-2021 VINCENT WIDMER
+
+// This file is part of Beekee Live.
+    
+// Beekee Live is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Beekee Live is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+//  along with Beekee Live.  If not, see <https://www.gnu.org/licenses/>.
+
+//**************************************************************************
+
+
 Template.homePost.events({
 
 	'click .home-post--order-up': function(e) {
